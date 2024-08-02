3c241144fec0b1ad50bd514f7f4c1c3d614a17bc
Valentin LE BESCOND <valentin.lebescond@univ-eiffel.fr>
2024-08-02 15:39:27 +0200

    use classes in tests

--- tests/test_signal.py
+++ tests/test_signal.py
@@ -1,21 +1,45 @@
 """
 Tests for :func:`acoustic_toolbox.signal`
 """
+
 from acoustic_toolbox.signal import convolve as convolveLTV
 from scipy.signal import convolve as convolveLTI
 import numpy as np
-import itertools
 
-from acoustic_toolbox.signal import *  #decibel_to_neper, neper_to_decibel, ir2fr, zero_crossings
-from numpy.testing import assert_almost_equal, assert_array_almost_equal, assert_array_equal, assert_approx_equal
+from acoustic_toolbox.signal import (
+    EqualBand,
+    OctaveBand,
+    decibel_to_neper,
+    neper_to_decibel,
+    ir2fr,
+    zero_crossings,
+    integrate_bands,
+    bandpass_octaves,
+    bandpass_third_octaves,
+    bandpass_fractional_octaves,
+    bandpass_frequencies,
+    amplitude_envelope,
+    rms,
+    ms,
+)
+from numpy.testing import assert_array_almost_equal, assert_array_equal
 
 import pytest
 
 
-@pytest.mark.parametrize("u,h", [
-    (np.array([1, 2, 3, 4, 3, 2, 1], dtype='float'), np.array([1, 2, 3, 4], dtype='float')),
-    (np.array([1, 2, 3, 4, 3, 2, 1, 1], dtype='float'), np.array([1, 2, 3, 4, 5], dtype='float')),
-])
+@pytest.mark.parametrize(
+    "u,h",
+    [
+        (
+            np.array([1, 2, 3, 4, 3, 2, 1], dtype="float"),
+            np.array([1, 2, 3, 4], dtype="float"),
+        ),
+        (
+            np.array([1, 2, 3, 4, 3, 2, 1, 1], dtype="float"),
+            np.array([1, 2, 3, 4, 5], dtype="float"),
+        ),
+    ],
+)
 def test_convolve_lti(u, h):
     """Test whether :func:`acoustic_toolbox.signal.convolve` behaves properly when
     performing a convolution with a time-invariant system.
@@ -23,10 +47,14 @@ def test_convolve_lti(u, h):
     H = np.tile(h, (len(u), 1)).T
 
     np.testing.assert_array_almost_equal(convolveLTV(u, H), convolveLTI(u, h))
-    np.testing.assert_array_almost_equal(convolveLTV(u, H, mode='full'), convolveLTI(u, h, mode='full'))
-    np.testing.assert_array_almost_equal(convolveLTV(u, H, mode='valid'), convolveLTI(u, h, mode='valid'))
+    np.testing.assert_array_almost_equal(
+        convolveLTV(u, H, mode="full"), convolveLTI(u, h, mode="full")
+    )
+    np.testing.assert_array_almost_equal(
+        convolveLTV(u, H, mode="valid"), convolveLTI(u, h, mode="valid")
+    )
     # This function and test needs some investigation. Disabling for now.
-    #np.testing.assert_array_almost_equal(convolveLTV(u,H,mode='same'), convolveLTI(u,h,mode='same'))
+    # np.testing.assert_array_almost_equal(convolveLTV(u,H,mode='same'), convolveLTI(u,h,mode='same'))
 
 
 def test_convolve_ltv():
@@ -45,11 +73,11 @@ def test_convolve_ltv():
 
 
 def test_decibel_to_neper():
-    assert (decibel_to_neper(1.0) == 0.11512925464970229)
+    assert decibel_to_neper(1.0) == 0.11512925464970229
 
 
 def test_neper_to_decibel():
-    assert (neper_to_decibel(1.0) == 8.685889638065035)
+    assert neper_to_decibel(1.0) == 8.685889638065035
 
 
 def test_ir2fr():
@@ -60,11 +88,11 @@ def test_ir2fr():
     t = 1.0
     fs = 100.0
     f = 20.0
-    ts = np.arange(0, t, 1. / fs)
+    ts = np.arange(0, t, 1.0 / fs)
 
     A = 5.0
 
-    x = A * np.sin(2. * np.pi * f * ts)
+    x = A * np.sin(2.0 * np.pi * f * ts)
 
     fv, fr = ir2fr(x, fs)
 
@@ -73,7 +101,7 @@ def test_ir2fr():
     assert_array_almost_equal(np.abs(fr).max(), A)
 
 
-class TestEqualBand:  #(unittest.TestCase):
+class TestEqualBand:  # (unittest.TestCase):
     """
     Test :class:`acoustic_toolbox.signal.EqualBand`.
     """
@@ -121,28 +149,25 @@ class TestEqualBand:  #(unittest.TestCase):
         assert_array_equal(b.center, x)
 
     def test_selection(self):
-
         eb = EqualBand(fstart=0.0, fstop=10.0, nbands=100)
         assert type(eb[3] == type(eb))
         assert type(eb[3:10] == type(eb))
 
 
-class Test_integrate_bands():
+class Test_integrate_bands:
     """
     Test :func:`acoustic_toolbox.signal.test_integrate_bands`.
     """
 
     def test_narrowband_to_octave(self):
-
-        nb = EqualBand(np.arange(100, 900, 200.))
+        nb = EqualBand(np.arange(100, 900, 200.0))
         x = np.ones(len(nb))
-        ob = OctaveBand(([125., 250, 500.]))
+        ob = OctaveBand(([125.0, 250, 500.0]))
         y = integrate_bands(x, nb, ob)
         assert_array_equal(y, np.array([1, 1, 2]))
 
 
 def test_zero_crossings():
-
     duration = 2.0
     fs = 44100.0
     f = 1000.0
@@ -153,15 +178,14 @@ def test_zero_crossings():
     z = zero_crossings(x)
 
     # Amount of zero crossings.
-    assert (len(z) == f * duration * 2)
+    assert len(z) == f * duration * 2
 
     # Position of zero crossings.
     y = np.linspace(0, samples, len(z), endpoint=False).astype(int)
-    assert ((np.abs(z - y) <= 1).all())
+    assert (np.abs(z - y) <= 1).all()
 
 
 def test_ms():
-
     duration = 2.0
     fs = 8000.0
     f = 1000.0
@@ -169,15 +193,14 @@ def test_ms():
     t = np.arange(samples) / fs
     x = np.sin(2.0 * np.pi * f * t)
 
-    assert (np.abs(ms(x) - 0.5) < 1e-9)
+    assert np.abs(ms(x) - 0.5) < 1e-9
 
     x *= 4.0
 
-    assert (np.abs(ms(x) - 8.0) < 1e-9)
+    assert np.abs(ms(x) - 8.0) < 1e-9
 
 
 def test_rms():
-
     duration = 2.0
     fs = 8000.0
     f = 1000.0
@@ -185,11 +208,11 @@ def test_rms():
     t = np.arange(samples) / fs
     x = np.sin(2.0 * np.pi * f * t)
 
-    assert (np.abs(rms(x) - np.sqrt(0.5)) < 1e-9)
+    assert np.abs(rms(x) - np.sqrt(0.5)) < 1e-9
 
     x *= 4.0
 
-    assert (np.abs(rms(x) - np.sqrt(8.0)) < 1e-9)
+    assert np.abs(rms(x) - np.sqrt(8.0)) < 1e-9
 
 
 @pytest.fixture(params=[4000.0, 20000.0, 44100.0])
@@ -208,8 +231,7 @@ def frequency(request):
 
 
 def test_amplitude_envelope(amplitude, frequency, fs):
-    """Test amplitude envelope.
-    """
+    """Test amplitude envelope."""
     duration = 5.0
     samples = int(fs * duration)
     t = np.arange(samples) / fs
@@ -221,23 +243,23 @@ def test_amplitude_envelope(amplitude, frequency, fs):
     # there is also a zero.
     amplitude_determined = np.unique(np.round(out), 6)[0]
 
-    assert (amplitude == amplitude_determined)
+    assert amplitude == amplitude_determined
 
 
-#def test_instantaneous_frequency(amplitude, frequency, fs):
+# def test_instantaneous_frequency(amplitude, frequency, fs):
 
-#duration = 5.0
-#samples = int(fs*duration)
-#t = np.arange(samples) / fs
+# duration = 5.0
+# samples = int(fs*duration)
+# t = np.arange(samples) / fs
 
-#signal = amplitude * np.sin(2.0*np.pi*frequency*t)
+# signal = amplitude * np.sin(2.0*np.pi*frequency*t)
 
-#out = instantaneous_frequency(signal, fs)
+# out = instantaneous_frequency(signal, fs)
 ## Rounding is necessary. We take the first element because occasionally
 ## there is also a zero.
-#frequency_determined = np.unique(np.round(out), 0)
+# frequency_determined = np.unique(np.round(out), 0)
 
-#assert( frequency == frequency_determined )
+# assert( frequency == frequency_determined )
 
 
 @pytest.mark.parametrize("channels", [1, 2, 5])
