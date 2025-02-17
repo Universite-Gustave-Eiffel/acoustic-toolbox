f3df61988ad26fe6e3f178c82a5791c88b77f3d5
Andrew Mitchell <a.j.mitchell@ucl.ac.uk>
2025-02-17 19:29:53 +0000

    convert docs and typehint

--- acoustic_toolbox/quantity.py
+++ acoustic_toolbox/quantity.py
@@ -1,54 +1,42 @@
-"""
-Quantities and units
-====================
-
-The Quantity module provides two classes to work with quantities and units.
-
-
-.. inheritance-diagram:: acoustic_toolbox.quantity
-
-"""
+"""The Quantity module provides two classes to work with quantities and units."""
 
 from acoustic_toolbox.standards.iso_tr_25417_2007 import REFERENCE_PRESSURE
 
 quantities = {
     "pressure": ("Pressure", "pascal", True, "p", "$p$", REFERENCE_PRESSURE),
 }
-"""
-Dictionary with quantities. Each quantity is stored as a tuple.
-"""
+"""Dictionary with quantities. Each quantity is stored as a tuple."""
 
 units = {
     "meter": ("meter", "m", "$m$"),
     "pascal": ("pascal", "Pa", "$Pa$"),
 }
-"""
-Dictionary with units. Each unit is stored as a tuple.
-"""
+"""Dictionary with units. Each unit is stored as a tuple."""
 
 
 class Unit:
+    """Unit of quantity.
+
+    Note:
+      Perhaps inherit from tuple or [`collections.namedTuple`][collections.namedTuple]?
     """
-    Unit of quantity.
 
-    .. note:: Perhaps inherit from tuple or :class:`collections.namedTuple`?
+    def __init__(self, name: str, symbol: str, symbol_latex: str):
+        """Initialize the unit.
 
-    """
-
-    def __init__(self, name, symbol, symbol_latex):
+        Args:
+          name: Name of the unit.
+          symbol: Symbol of the unit.
+          symbol_latex: Symbol of the unit in LaTeX.
+        """
         self.name = name
-        """
-        Name of the unit.
-        """
+        """Name of the unit."""
+
         self.symbol = symbol
-        """
-        Symbol of the unit.
-        """
+        """Symbol of the unit."""
 
         self.symbol_latex = symbol_latex
-        """
-        Symbol of the unit in LaTeX.
-        """
+        """Symbol of the unit in LaTeX."""
 
     def __repr__(self):
         return "Unit({})".format(self.name)
@@ -58,42 +46,44 @@ class Unit:
 
 
 class Quantity:
-    """
-    Quantity.
-    """
+    """Quantity."""
 
     def __init__(
-        self, name, unit, dynamic, symbol=None, symbol_latex=None, reference=1.0
+        self,
+        name: str,
+        unit: Unit,
+        dynamic: bool,
+        symbol: str | None = None,
+        symbol_latex: str | None = None,
+        reference: float = 1.0,
     ):
+        """Initialize the quantity.
+
+        Args:
+          name: Name of the quantity.
+          unit: Unit of the quantity.
+          dynamic: Dynamic quantity (`True`) or energetic (`False`).
+          symbol: Symbol of the quantity.
+          symbol_latex: Symbol of the quantity in LaTeX.
+          reference: Reference value of the quantity.
+        """
         self.name = name
-        """
-        Name of the quantity.
-        """
+        """Name of the quantity."""
 
         self.symbol = symbol
-        """
-        Symbol of the quantity.
-        """
+        """Symbol of the quantity."""
 
         self.symbol_latex = symbol_latex
-        """
-        Symbol of the unit in LaTeX.
-        """
+        """Symbol of the unit in LaTeX."""
 
         self.unit = unit
-        """
-        Unit. See :class:`Unit`.
-        """
+        """Unit. See [`Unit`][acoustic_toolbox.quantity.Unit]."""
 
         self.dynamic = dynamic
-        """
-        Dynamic quantity (`True`) or energetic (`False`).
-        """
+        """Dynamic quantity (`True`) or energetic (`False`)."""
 
         self.reference = reference
-        """
-        Reference value of the quantity.
-        """
+        """Reference value of the quantity."""
 
     def __repr__(self):
         return "Quantity({})".format(self.name)
@@ -102,19 +92,19 @@ class Quantity:
         return self.name
 
     @property
-    def energetic(self):
-        """
-        Energetic quantity (`True`) or dynamic (`False`).
-        """
+    def energetic(self) -> bool:
+        """Energetic quantity (`True`) or dynamic (`False`)."""
         return not self.dynamic
 
 
-def get_quantity(name):
-    """
-    Get quantity by name. Returns instance of :class:`Quantity`.
+def get_quantity(name: str) -> Quantity:
+    """Get quantity by name. Returns instance of [`Quantity`][acoustic_toolbox.quantity.Quantity].
 
-    :param name: Name of the quantity.
+    Args:
+      name: Name of the quantity.
 
+    Returns:
+      Instance of [`Quantity`][acoustic_toolbox.quantity.Quantity].
     """
     try:
         q = list(quantities[name])
---
+++ docs/quantity.md
@@ -0,0 +1,8 @@
+# Quantities and Units
+
+::: acoustic_toolbox.quantity
+    options:
+        show_root_heading: false
+        show_source: true
+        show_docstring_yields: true
+        show_inheritance_diagram: true
