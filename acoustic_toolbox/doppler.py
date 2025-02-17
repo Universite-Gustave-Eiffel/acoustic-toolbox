ca45050184482766e13600fc67b1ad64d1de78d5
Andrew Mitchell <a.j.mitchell@ucl.ac.uk>
2025-02-17 01:50:30 +0000

    start converting docs

--- acoustic_toolbox/doppler.py
+++ acoustic_toolbox/doppler.py
@@ -1,43 +1,48 @@
 """
-Doppler
-=======
-
 Doppler shift module.
 """
 
 SOUNDSPEED = 343.0
-"""Speed of sound
-"""
+"""Speed of sound"""
 
 
-def velocity_from_doppler_shift(f1, f2, c=SOUNDSPEED):
-    r"""Calculate velocity based on measured frequency shifts due to Doppler shift.
+def velocity_from_doppler_shift(f1, f2, c=SOUNDSPEED) -> float:
+    """Calculate velocity based on measured frequency shifts due to Doppler shift.
 
-    :param c: Speed of sound :math:`c`.
-    :param f1: Lower frequency :math:`f_1`.
-    :param f2: Upper frequency :math:`f_2`.
-
-    .. math:: v = c \cdot \left( \frac{f_2 - f_1}{f_2 + f_1} \right)
+    $$
+    v = c \\cdot \\left( \\frac{f_2 - f_1}{f_2 + f_1} \\right)
+    $$
 
     The assumption is made that the velocity is constant between the observation times.
 
+    Args:
+      f1: Lower frequency $f_1$.
+      f2: Upper frequency $f_2$.
+      c: Speed of sound $c$.
+
+    Returns:
+        Calculated velocity.
     """
     return c * (f2 - f1) / (f2 + f1)
 
 
 def frequency_shift(
     frequency, velocity_source, velocity_receiver, soundspeed=SOUNDSPEED
-):
-    r"""Frequency shift due to Doppler effect.
+) -> float:
+    """Frequency shift due to Doppler effect.
 
-    :param frequency: Emitted frequency :math:`f`.
-    :param velocity_source: Velocity of source :math:`v_s`.
+    Args:
+      frequency: Emitted frequency $f$.
+      velocity_source: Velocity of source $v_s$.
         Positive if the source is moving away from the receiver (and negative in the other direction).
-    :param velocity_receiver: Velocity of receiver :math:`v_r`.
+      velocity_receiver: Velocity of receiver $v_r$.
         Positive if the receiver is moving towards the source (and negative in the other direction);
-    :param soundspeed: Speed of sound :math:`c`.
+      soundspeed: Speed of sound $c$.
+        $$
+        f = \\frac{c + v_r}{c + v_s} f_0
+        $$
 
-    .. math:: f = \frac{c + v_r}{c + v_s} f_0
-
+    Returns:
+        Frequency after shift.
     """
     return (soundspeed + velocity_receiver) / (soundspeed + velocity_source) * frequency
